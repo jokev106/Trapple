8fece512dcab8397a43306be943a0cfa9ef38bb0
vleonard <vleonard@student.ciputra.ac.id>
2022-08-23 16:07:48 +0700

    SwiftUI Creation

---
+++ Trapple/Trapple/TrappleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrappleApp.swift
+//  Trapple
+//
+//  Created by Vincent Leonard on 23/08/22.
+//
+
+import SwiftUI
+
+@main
+struct TrappleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
