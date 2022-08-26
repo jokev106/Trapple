174bfc8688b0049f94e88e2e5b4b3de20f1c3ecd
jokev106 <62888793+jokev106@users.noreply.github.com>
2022-08-26 15:58:41 +0700

    Feat: Update Create Plan view Lofi

---
+++ Trapple/Trapple/Views/CreatePlanView.swift
@@ -0,0 +1,20 @@
+//
+//  CreatePlanView.swift
+//  Trapple
+//
+//  Created by Jonathan Kevin on 26/08/22.
+//
+
+import SwiftUI
+
+struct CreatePlanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreatePlanView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreatePlanView()
+    }
+}
