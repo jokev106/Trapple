610ea64cd1bce8638b572509697ec3f23f6d294e
sirjvp <jonathanvalentinoprawira@yahoo.com>
2022-08-28 18:18:37 +0700

    Update: Custom Fonts + Colors

--- Trapple/Trapple/Views/TripHomePageView.swift
+++ Trapple/Trapple/Views/TripHomePageView.swift
@@ -11,148 +11,158 @@ struct TripHomePageView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-                // RUNDOWN
-                VStack(spacing: 0) {
-                    VStack(alignment: .leading) {
-                        Text("Rundown")
-                            .font(.title2)
-                            .fontWeight(.bold)
-                        Text("Description")
-                        Text("Days : From - Until")
-                            .opacity(0.5)
-                    }
-                    .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .background(.gray)
-                    .cornerRadius(15)
-
-                    HStack {
-                        VStack {
-                            Spacer()
-                            Text("00:00")
-                                .font(.headline)
-                        }
-                        .padding(10)
-                        
-                        Rectangle()
-                            .frame(width: 1)
-                            .background(.black)
-                        
-                        VStack(alignment: .leading) {
-                            Text("Activity")
-                                .font(.headline)
-                            Text("Location")
-                                .font(.caption)
-                        }
-                        
-                        .frame(maxWidth: 200, alignment: .leading)
-                        .padding(10)
-                    }
-                    
-                    .frame(maxWidth: .infinity)
-                    .background(.yellow)
-                    .cornerRadius(10)
-                    .padding()
+                VStack {
+                    Rundown
+                    Equipment
+                    Rules
                 }
-                .background(.orange)
-                .cornerRadius(15)
-                .padding()
-                
-                // EQUIPMENT
-                VStack(spacing: 0) {
-                    VStack(alignment: .leading) {
-                        Text("Equipment")
-                            .font(.title2)
-                            .fontWeight(.bold)
-                    }
-                    .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .background(.gray)
-                    .cornerRadius(15)
-
-                    HStack(spacing: 0) {
-                        Text("Item1")
-                            .font(.headline)
-                            .padding(10)
-                        
-                        Spacer()
-                        
-                        ZStack {
-                            Rectangle()
-                                .frame(width: 20, height: 20)
-                                .foregroundColor(.blue)
-                                .cornerRadius(5)
-                                .padding(10)
-                            
-                            Image(systemName: "checkmark")
-                                .resizable()
-                                .frame(width: 10, height: 10)
-                                .foregroundColor(.white)
-                        }
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .background(.yellow)
-                    .cornerRadius(10)
-                    .padding()
-                }
-                .background(.orange)
-                .cornerRadius(15)
-                .padding()
-                
-                // RULES
-                VStack(spacing: 0) {
-                    VStack(alignment: .leading) {
-                        Text("Rules")
-                            .font(.title2)
-                            .fontWeight(.bold)
-                    }
-                    .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .background(.gray)
-                    .cornerRadius(15)
-
-                    HStack(spacing: 0) {
-                        Rectangle()
-                            .frame(width: 5)
-                            .background(.black)
-                            .cornerRadius(15)
-                            .padding(.trailing)
-                            .opacity(0.4)
-                        
-                        VStack(alignment: .leading) {
-                            Text("Rules Number One")
-                                .font(.headline)
-                        }
-                        
-                        .frame(maxWidth: 200, alignment: .leading)
-                        .padding(10)
-                    }
-                    
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding()
-                }
-                .background(.orange)
-                .cornerRadius(15)
-                .padding()
-                
-                Button(action: {}, label: {
-                    Text("Finish Trip")
-                        .foregroundColor(.white)
-                        .font(.headline)
-                })
-                .padding()
-                .frame(maxWidth: .infinity)
-                .background(.blue)
-                .cornerRadius(15)
-                .padding()
             }
-            .navigationBarTitle("Trip Name")
+            .background(Color("grayBG"))
+            .font(Font.custom("Gilroy-Light", size: 20))
+            .navigationTitle("Trip Name")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {}, label: {
+                        Image(systemName: "square.and.arrow.up")
+                            .foregroundColor(.black)
+                    })
+                }
+            }
         }
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
 struct TripHomePageView_Previews: PreviewProvider {
     static var previews: some View {
         TripHomePageView()
+    }
+}
+
+// MARK: Components
+extension TripHomePageView {
+    private var Rundown: some View {
+        VStack(spacing: 0) {
+            NavigationLink(destination: RundownView(), label: {
+                VStack(alignment: .leading) {
+                    Text("Rundown")
+                        .fontWeight(.bold)
+                    Text("Description".uppercased())
+                        .font(Font.custom("Gilroy-Light", size: 18))
+                    Text("Days : From - Until")
+                        .font(Font.custom("Gilroy-Light", size: 15))
+                        .opacity(0.5)
+                }
+                .foregroundColor(.black)
+                .padding()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color("yellowCard"))
+                .cornerRadius(15)
+            })
+
+            HStack(spacing: 0) {
+                Text("00:00")
+                    .font(Font.custom("Gilroy-Light", size: 15))
+                    .fontWeight(.bold)
+                    .padding()
+                
+                Rectangle()
+                    .frame(width: 1)
+                    .foregroundColor(Color("yellowCard"))
+                
+                VStack(alignment: .leading) {
+                    Text("Activity")
+                    Text("Location")
+                        .font(Font.custom("Gilroy-Light", size: 15))
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(10)
+                .padding(.leading)
+            }
+            .frame(maxWidth: .infinity)
+            .background(.white)
+            .cornerRadius(15)
+            .shadow(radius: 1)
+            .padding()
+        }
+        .background(.white)
+        .cornerRadius(15)
+        .padding()
+    }
+    
+    private var Equipment: some View {
+        VStack(spacing: 0) {
+            VStack(alignment: .leading) {
+                Text("Equipment")
+                    .fontWeight(.bold)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color("yellowCard"))
+            .cornerRadius(15)
+
+            HStack(spacing: 0) {
+                Text("Item1")
+                    .padding(10)
+            
+                Spacer()
+            
+                ZStack {
+                    Rectangle()
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(.blue)
+                        .cornerRadius(5)
+                        .padding(10)
+                
+                    Image(systemName: "checkmark")
+                        .resizable()
+                        .frame(width: 10, height: 10)
+                        .foregroundColor(.white)
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(.white)
+            .cornerRadius(15)
+            .shadow(radius: 1)
+            .padding()
+        }
+        .background(.white)
+        .cornerRadius(15)
+        .padding()
+    }
+    
+    private var Rules: some View {
+        VStack(spacing: 0) {
+            VStack(alignment: .leading) {
+                Text("Rules")
+                    .fontWeight(.bold)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color("yellowCard"))
+            .cornerRadius(15)
+
+            HStack(spacing: 0) {
+                Rectangle()
+                    .frame(width: 5)
+                    .background(.black)
+                    .cornerRadius(15)
+                    .padding(.trailing)
+                    .opacity(0.4)
+            
+                VStack(alignment: .leading) {
+                    Text("Rules Number One")
+                }
+            
+                .frame(maxWidth: 200, alignment: .leading)
+                .padding(10)
+            }
+        
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding()
+        }
+        .background(.white)
+        .cornerRadius(15)
+        .padding()
     }
 }
