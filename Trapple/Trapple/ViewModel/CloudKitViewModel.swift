7843fdbfdd429376a1333b8bc64392445f368ab2
vleonard <vleonard@student.ciputra.ac.id>
2022-08-31 14:30:49 +0700

    Update: CloudKitViewModel

--- Trapple/Trapple/ViewModel/CloudKitViewModel.swift
+++ Trapple/Trapple/ViewModel/CloudKitViewModel.swift
@@ -1,8 +1,81 @@
 //
-//  CloudKitViewModel.swift
+//  CloudkitViewModel.swift
 //  Trapple
 //
-//  Created by Vincent Leonard on 31/08/22.
+//  Created by Vincent Leonard on 25/08/22.
 //
 
-import Foundation
+import SwiftUI
+import CloudKit
+
+class CloudKitViewModel: ObservableObject {
+    
+    @Published var permissionStatus: Bool = false
+    @Published var isSignedInToiCloud: Bool = false
+    @Published var error: String = ""
+    @Published var userName: String = ""
+    
+    init(){
+        getiCloudStatus()
+        requestPermission()
+        fetchiCloudUserRecordID()
+    }
+    
+    private func getiCloudStatus(){
+        CKContainer.default().accountStatus { [weak self] returnedStatus, returnedError in
+            DispatchQueue.main.sync {
+                switch returnedStatus {
+                case .couldNotDetermine:
+                    self?.error = CloudKitError.iCloudAccountNotDetermined.rawValue
+                case .available:
+                    self?.isSignedInToiCloud = true
+                case .restricted:
+                    self?.error = CloudKitError.iCloudAccountRestricted.rawValue
+                case .noAccount:
+                    self?.error = CloudKitError.iCloudAccountNotFound.rawValue
+                case .temporarilyUnavailable:
+                    break
+                default:
+                    self?.error = CloudKitError.iCloudAccountUnknown.rawValue
+                }
+            }
+        }
+        
+    }
+    
+    enum CloudKitError: String, LocalizedError{
+        case iCloudAccountNotFound
+        case iCloudAccountNotDetermined
+        case iCloudAccountRestricted
+        case iCloudAccountUnknown
+        
+    }
+    
+    func requestPermission() {
+        CKContainer.default().requestApplicationPermission([.userDiscoverability]) { [weak self] returnedStatus, returnedError in
+            DispatchQueue.main.async {
+                if returnedStatus == .granted {
+                    self?.permissionStatus = true
+                }
+            }
+        }
+    }
+    
+    func fetchiCloudUserRecordID() {
+        CKContainer.default().fetchUserRecordID { returnedID, returnedError in
+            if let id = returnedID {
+                self.discoveriCloudUser(id: id)
+            }
+        }
+    }
+    
+    func discoveriCloudUser(id: CKRecord.ID){
+        CKContainer.default().discoverUserIdentity(withUserRecordID: id) { [weak self] returnedIdentity, returnedError in
+            DispatchQueue.main.async {
+                if let name = returnedIdentity?.nameComponents?.givenName{
+                    self?.userName = name
+                }
+            }
+        }
+    }
+}
