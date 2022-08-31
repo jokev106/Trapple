26cd0e53c0ea5a772babf437e3ce2b193a12659b
vleonard <vleonard@student.ciputra.ac.id>
2022-08-31 14:43:30 +0700

    Update: ClourKitTesting

--- Trapple/Trapple/Views/CloudKitTesting/CloudKitTesting.swift
+++ Trapple/Trapple/Views/CloudKitTesting/CloudKitTesting.swift
@@ -1,8 +1,33 @@
 //
-//  CloudKitTesting.swift
+//  CloudkitTesting.swift
 //  Trapple
 //
-//  Created by Vincent Leonard on 31/08/22.
+//  Created by Vincent Leonard on 25/08/22.
 //
 
-import Foundation
+import SwiftUI
+import CloudKit
+
+struct CloudKitTesting: View{
+    
+    @StateObject private var vm = CloudKitViewModel()
+    
+    var body: some View{
+        NavigationView{
+            VStack{
+                Text("Is Signed In: \(vm.isSignedInToiCloud.description.uppercased())")
+                Text(vm.error)
+                Text("Permission: \(vm.permissionStatus.description.uppercased())")
+                Text("Name: \(vm.userName)")
+                NavigationLink("Next", destination: CloudKitCRUD())
+            }
+        }
+        
+    }
+}
+
+struct CloudKitTesting_Previews: PreviewProvider {
+    static var previews: some View {
+        CloudKitTesting()
+    }
+}
