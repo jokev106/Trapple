af56130415173c08d20c3d8e3f31e04c60ab628f
vleonard <vleonard@student.ciputra.ac.id>
2022-09-09 13:46:09 +0700

    Update: Fixed Rundown Date

--- Trapple/Trapple/Views/RundownView.swift
+++ Trapple/Trapple/Views/RundownView.swift
@@ -24,6 +24,7 @@ struct RundownView: View {
     @State private var dates = ["August 3", "August 4", "August 5", "August 6", "August 7", "August 8", "August 9"]
     @State private var isExist = true
     @State private var apaa = CKRecord.ID(recordName: "0")
+    @State private var currentDate: String = ""
     
     var body: some View {
         GeometryReader { geometry in
@@ -33,9 +34,15 @@ struct RundownView: View {
                 ScrollView {
                     VStack(spacing: 0) {
                         HStack {
-                            Text("3 September")
+                            Text(currentDate)
                                 .foregroundColor(.black)
                                 .font(Font.custom("Gilroy-ExtraBold", size: 17))
+                                .onAppear{
+                                    let dateFormatter = DateFormatter()
+                                    dateFormatter.dateFormat = "MMMM d"
+                                    let startDatestring = dateFormatter.string(from: startDate)
+                                    currentDate = startDatestring
+                                }
                             
                             Spacer()
                             
@@ -124,6 +131,7 @@ extension RundownView {
                     ForEach(vm.dates.indices, id: \.self) { index in
                         Button(action: { selected = index
                             print(selected)
+                            currentDate = vm.dates[selected]
                             vm.fetchItems(planID: planID, actualDate: vm.dates[selected])
                         }) {
                             VStack {
@@ -132,10 +140,10 @@ extension RundownView {
                                     .foregroundColor(.black)
                                     .opacity(selected == index ? 1.0 : 0.3)
                             
-                                Text(vm.dates[index])
-                                    .foregroundColor(.black)
-                                    .font(Font.custom("Gilroy-Light", size: 13))
-                                    .opacity(selected == index ? 0.4 : 0.2)
+//                                Text(vm.dates[index])
+//                                    .foregroundColor(.black)
+//                                    .font(Font.custom("Gilroy-Light", size: 13))
+//                                    .opacity(selected == index ? 0.4 : 0.2)
                             
                                 Spacer()
                             
@@ -154,6 +162,7 @@ extension RundownView {
                 // LEFT
                 Button(action: {
                     slider -= 1; posX += 300; selected = (slider * 3)
+                    currentDate = vm.dates[selected]
                     vm.fetchItems(planID: planID, actualDate: vm.dates[selected])
                 }) {
                     if slider > 0 {
@@ -169,6 +178,7 @@ extension RundownView {
                 // RIGHT
                 Button(action: {
                     slider += 1; posX -= 300; selected = (slider * 3)
+                    currentDate = vm.dates[selected]
                     vm.fetchItems(planID: planID, actualDate: vm.dates[selected])
                 }) {
                     if slider < Int(ceil(Double(vm.dates.count) / 3) - 1) {
